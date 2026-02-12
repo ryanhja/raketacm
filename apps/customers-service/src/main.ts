62fa010cff611c5f1eb887cc8c6658c2b3cfba97
ryanhja <rayanstive9@gmail.com>
2026-02-12 22:42:15 +0300

    feat(microservices) Add customers and users services

---
+++ apps/customers-service/src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+    },
+  );
+
+  await app.listen();
+}
+
+bootstrap();
