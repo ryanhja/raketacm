a40306a72a0f0fef1d1eecddc0cedccc90e138ae
ryanhja <rayanstive9@gmail.com>
2026-02-14 21:39:00 +0300

    feat(Users): Add get users

--- apps/users-service/src/app/users/users.controller.ts
+++ apps/users-service/src/app/users/users.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get('user')
+  async getAllUser(): Promise<string> {
+    let data = await this.usersService.getUsers();
+
+    return data;
+  }
 }
