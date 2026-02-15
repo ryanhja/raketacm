61feabdd063b3252c6de034c3fafe357c85dd558
ryanhja <rayanstive9@gmail.com>
2026-02-15 22:40:58 +0300

    feat(users): Add admin resource

--- apps/users-service/src/app/users/users.controller.ts
+++ apps/users-service/src/app/users/users.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get('user')
+  @MessagePattern('users')
   async getAllUser(): Promise<string> {
-    let data = await this.usersService.getUsers();
+    const data = await this.usersService.getUsers();
 
     return data;
   }
